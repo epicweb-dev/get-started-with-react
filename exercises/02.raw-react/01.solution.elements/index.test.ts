8bac9cc35869088e91a5574dbf1d831147dedaf0
Kent C. Dodds <me+github@kentcdodds.com>
2024-08-19 17:02:12 -0600

    finish tests

--- exercises/02.raw-react/01.solution.elements/index.test.ts
+++ exercises/02.raw-react/01.solution.elements/index.test.ts
@@ -1,11 +1,11 @@
 import { expect, testStep } from '@epic-web/workshop-utils/test'
 
-await testStep('Hello is rendered to the DOM', () => {
+await testStep('"Hello World" is rendered to the DOM', () => {
 	const rootElement = document.getElementById('root')
 	expect(rootElement, 'root element not found').to.be.instanceOf(HTMLElement)
 
 	const element = rootElement!.querySelector('.container')
-	expect(element, 'element not found').to.be.instanceOf(HTMLElement)
+	expect(element, 'container element not found').to.be.instanceOf(HTMLElement)
 
 	expect(element!.textContent, 'element text is not correct').to.equal(
 		'Hello World',
--- exercises/03.using-jsx/03.solution.spread/content.test.ts
+++ exercises/03.using-jsx/03.solution.spread/content.test.ts
@@ -1,5 +1,8 @@
 import { expect, testStep } from '@epic-web/workshop-utils/test'
 
+// wait for babel to compile and evaluate the JSX
+await new Promise(resolve => setTimeout(resolve, 100))
+
 await testStep('"Hello World" is rendered to the DOM', () => {
 	const rootElement = document.getElementById('root')
 	expect(rootElement, 'root element not found').to.be.instanceOf(HTMLElement)
