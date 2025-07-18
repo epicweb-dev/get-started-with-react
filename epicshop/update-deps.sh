51249f1869ae727035d0c9de05787ce5aaadd96c
Kent C. Dodds <me+github@kentcdodds.com>
2025-07-17 21:16:10 -0600

    improve setup

--- epicshop/update-deps.sh
+++ epicshop/update-deps.sh
@@ -1,10 +1,10 @@
 set -e
 
-npx npm-check-updates --dep prod,dev --upgrade --root --reject react,react-dom
-cd epicshop && npx npm-check-updates --dep prod,dev --upgrade --root react,react-dom
+npx npm-check-updates --dep prod,dev --upgrade --root
+cd epicshop && npx npm-check-updates --dep prod,dev --upgrade --root
 cd ..
 rm -rf node_modules package-lock.json ./epicshop/package-lock.json ./epicshop/node_modules ./exercises/**/node_modules
 npm install
 npm run setup
 npm run typecheck
-npm run lint --fix
+npm run lint -- --fix
